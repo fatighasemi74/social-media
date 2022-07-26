e5d7c4f3088d6c956f1cf51dc12117ed09a2b4e6
Fati <fati@Fatis-MacBook-Air.local>
2022-07-26 16:26:30 +0430

    register in User and UserAccount

--- account/models.py
+++ account/models.py
@@ -3,9 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserAccount(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=True)
-    # password = models.CharField(max_length=30)
+    username = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='user_account')
+    name = models.CharField(max_length=200, null=True, blank=True)
     profile_picture = models.ImageField(upload_to=None,null=True, blank=True, height_field=None, width_field=None, max_length=100)
     bio = models.TextField(blank=True, null= True)
     email = models.EmailField(max_length=254)
--- account/serializers.py
+++ account/serializers.py
@@ -12,25 +12,26 @@ class UserAccountCreateSerializer(serializers.ModelSerializer):
         fields = ('name', 'password',  'profile_picture', 'email', 'birth_date', 'bio')
         extra_kwargs = {'password': {'write_only': True}}
 
+    def get_password(self, password):
+        return password
 
     def create(self, validated_data):
-        # print(validated_data)
         password = validated_data.pop('password')
-        user = User.objects.create_user(validated_data['name'])
-        name = validated_data['name']
-        useraccount = UserAccount.objects.create(name=name)
+        user = User.objects.create_user(username=validated_data['name'])
         user.set_password(password)
         user.save()
-        # useraccount.save()
+        useraccount = UserAccount.objects.create(
+                                                 name=validated_data['name'],
+                                                 profile_picture=validated_data['profile_picture'],
+                                                 email=validated_data['email'],
+                                                 birth_date=validated_data['birth_date'],
+                                                 bio=validated_data['bio'])
+        useraccount.username = user
+        useraccount.save()
         return  useraccount
 
 
-    # def create_useraccount(self, validated_data):
-    #     print(validated_data)
-    #     return validated_data
 
-    def get_password(self, password):
-        return password
     #
     # def validate_password(self, password, user=None):
     #     if len(password) < self.min_length:
