64800e70d5ce62ef146f1bac0aa14a198db0c899
Fati <fati@191-1-1-240.user3g.veloxzone.com.br>
2022-08-01 15:24:23 +0430

    debug true

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#n%2h3=6hd_6j!qdor^za*2g=qh=7k2q#2vb5b)snw7hfud65u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['social-mediaaaa.herokuapp.com', '127.0.0.1']
 
