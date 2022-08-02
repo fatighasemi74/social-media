35a3da9d7abda76f11b4c6836b8ee9c5d8dfdd63
Fati <fati@191-1-0-141.user3g.veloxzone.com.br>
2022-08-02 12:10:41 +0430

    change middle ware

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -53,11 +53,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
