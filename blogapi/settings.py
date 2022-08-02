3cadbb0c5001546da2f27e5e56f322cafaecb41b
Fati <fati@191-1-0-141.user3g.veloxzone.com.br>
2022-08-02 12:04:43 +0430

    add port 3000 2

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -69,9 +69,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://social-mediaaaa.herokuapp.com",
-    "http://localhost:8080",
-    "http://localhost:3080",
-    "http://127.0.0.1:9000",
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
 ]
 
 CORS_ALLOW_HEADERS = [
