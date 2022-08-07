3ff1f6095867a1cb5e6881cd0b25dcbc7c2cadbc
Fati <fati@191-1-6-41.user3g.veloxzone.com.br>
2022-08-07 18:01:52 +0430

    create athenticate.py in account app

--- account/urls.py
+++ account/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
-from .views import UserCreateAPIView, LogoutView, MyTokenObtainPairView
+from .views import UserCreateAPIView, LogoutView, MyTokenObtainPairView, LoginView
 
 urlpatterns = [
     path('create/', UserCreateAPIView.as_view(), name='account-create'),
     # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
     path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify-token/', TokenVerifyView.as_view(), name='token_verify'),
     path('logout/', LogoutView.as_view(), name='logout'),
