b870e847a7f477d3d16b4320f8ec3dd6e255024b
Fati <fati@191-1-10-198.user3g.veloxzone.com.br>
2022-08-20 11:44:20 +0430

    title

--- content/serializers.py
+++ content/serializers.py
@@ -15,7 +15,7 @@ class PostListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         # fields = '__all__'
-        fields = ('id',  'caption', 'user', 'media', 'user_image')
+        fields = ('id','title',  'caption', 'user', 'media', 'user_image')
 
 
 
