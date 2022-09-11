9b66bfff2be2bb1079f50ca4768a2dc478933de8
Fati <fati@Fatis-MacBook-Air.local>
2022-09-11 09:54:18 +0430

    dropbox access token

--- content/views.py
+++ content/views.py
@@ -21,7 +21,7 @@ from relation.permissions import RelationExists, HasPostPermission
 
 
 class Pagination(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 5
 
 class PostCreateAPIView(generics.CreateAPIView):
 
